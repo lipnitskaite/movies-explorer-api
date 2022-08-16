fb450c215f574e6cb6bd6d06aa486ebeed3283bd
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2022-08-16 13:35:06 +0200

    feat: add authorization for routes

--- routes/routes.js
+++ routes/routes.js
@@ -9,14 +9,27 @@ const routes = require('express').Router();
 
 const { createUserValidation } = require('../middlewares/validation');
 
+const { auth } = require('../middlewares/auth');
+
 const { createUser } = require('../controllers/userController');
 
 const { usersRoutes } = require('./usersRoutes');
 const { moviesRoutes } = require('./moviesRoutes');
 
+const NotFoundError = require('../errors/NotFoundError');
+
 routes.post('/signup', createUserValidation, createUser);
+
+routes.use(auth);
 
 routes.use('/users', usersRoutes);
 routes.use('/movies', moviesRoutes);
+
+routes.all('*', (req, res, next) => {
+  throw new NotFoundError("Sorry, we can't find the page you're looking for.");
+
+  // eslint-disable-next-line no-undef, no-unreachable
+  next(err);
+});
 
 exports.routes = routes;
